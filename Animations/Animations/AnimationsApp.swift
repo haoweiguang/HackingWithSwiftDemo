b890dabd82c0c2eb75a8fd5311798ef43e795ff2
light.hao <haoweiguang1986@gmail.com>
2024-06-13 19:48:26 +0800

    新增了动画的工程
    Change-Id: I5d6ade30bf62d5de5f8134d66e15907dda1fe42e

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 郝伟光 on 2024/6/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
