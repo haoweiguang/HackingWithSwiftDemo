07f4fb74b7449ca373373b6a3c15dc23e41783c8
light.hao <haoweiguang1986@gmail.com>
2024-06-15 10:27:20 +0800

    一种涟漪的动画效果
    Change-Id: Id1cda2ecddec5f46330bd9767fc27668211145cc

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount=1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me"){
+            animationAmount+=1
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 2)
+                    .repeatForever(autoreverses:false),
+                    value: animationAmount
+                )
+        )
+        .onAppear(){
+            animationAmount=2
+        }
     }
 }
 
