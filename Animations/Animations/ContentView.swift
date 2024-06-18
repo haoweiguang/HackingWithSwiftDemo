debcc508e13ddfebbce75aadc104460028a85a81
light.hao <haoweiguang1986@gmail.com>
2024-06-18 14:21:43 +0800

    3d 效果
    Change-Id: I64563279d34f40719e875de8b8d25a7584958631

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -36,6 +36,50 @@ struct ContentView: View {
     }
 }
 
+struct AnimatedBindingsContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        print(animationAmount)
+        
+        return VStack{
+            Stepper("Scale amount",value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3,autoreverses: true)
+            ),in:1...10)
+            Spacer()
+            
+            Button("Tap Me"){
+                animationAmount+=1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+        }
+    }
+}
+
+
+struct ExplicitAnimationsContentView: View{
+    @State private var animationAmount = 0.0
+    
+    var body: some View{
+        Button("Tap me"){
+            withAnimation {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+    }
+}
+
 #Preview {
-    ContentView()
+    ExplicitAnimationsContentView()
 }
