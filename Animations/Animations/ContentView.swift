b4e8dd05ae0044b72564c827182702e28a60efef
light.hao <haoweiguang1986@gmail.com>
2024-06-20 14:39:06 +0800

    一些动画效果
    Change-Id: I33289f140d7fee087f280711d11e11bd6b5e6a71

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -80,6 +80,144 @@ struct ExplicitAnimationsContentView: View{
     }
 }
 
+struct AnimationStackContentView:View {
+    
+    @State private var enabled=false
+    
+    var body: some View {
+        Button("Tap Me"){
+            enabled.toggle()
+        }
+        .background(enabled ? .blue:.red)
+        .frame(width: 200,height: 200)
+        .foregroundColor(.white)
+        
+        Button("Tap Me"){
+            enabled.toggle()
+        }
+        .frame(width: 200,height: 200)
+        .background(enabled ? .blue:.red)
+        .foregroundColor(.white)
+        .animation(nil, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60:0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+    }
+}
+
+struct AnimatingGesturesContentView:View {
+    
+    @State private var dragAmount=CGSize.zero
+    
+    var body: some View {
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .frame(width: 300, height: 200)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .offset(dragAmount)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in
+                                withAnimation(.bouncy) {
+                                    dragAmount = .zero
+                                }
+                            }
+                    )
+                    .animation(.bouncy, value: dragAmount)
+        
+    }
+}
+
+struct AnimatingGestures1ContentView:View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled=false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+                    ForEach(0..<letters.count, id: \.self) { num in
+                        Text(String(letters[num]))
+                            .padding(5)
+                            .font(.title)
+                            .background(enabled ? .blue : .red)
+                            .offset(dragAmount)
+                            .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                    }
+                }
+                .gesture(
+                    DragGesture()
+                        .onChanged { dragAmount = $0.translation }
+                        .onEnded { _ in
+                            dragAmount = .zero
+                            enabled.toggle()
+                        }
+                )
+    }
+}
+
+struct TransitionsContentView:View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack{
+            Button("Tap Me"){
+                withAnimation{
+                    isShowingRed.toggle()
+                }
+            }
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
+
+struct CustomTransitionsContentView:View {
+    @State private var isShowingRed = false
+    
+    var body: some View{
+        ZStack{
+            Rectangle()
+                .fill(.red)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+            }
+        }
+    }
+}
+
+struct CornerRotateModifier : ViewModifier{
+    let amount:Double
+    let anchor:UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount),anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition{
+    static var pivot: AnyTransition{
+        .modifier(
+            active:CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity:CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 #Preview {
-    ExplicitAnimationsContentView()
+    CustomTransitionsContentView()
 }
